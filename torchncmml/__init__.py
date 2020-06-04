e387865ab9bb7580af4b55adc90183345673d2e6
Vinnam Kim <vinnamkim@gmail.com>
2020-06-04 12:20:03 +0900

    Integrate Loss and Transform into NCMML
    Signed-off-by: Vinnam Kim <vinnamkim@gmail.com>

--- torchncmml/__init__.py
+++ torchncmml/__init__.py
@@ -1,2 +1,2 @@
-from .ncmml import NCMMLLoss, LinearTransform
+from .ncmml import NCMML
 from .utils import get_class_mean
--- torchncmml/ncmml.py
+++ torchncmml/ncmml.py
@@ -1,21 +1,41 @@
 import torch
 from torch import nn
+from typing import Tuple
 
 
-class NCMMLLoss(nn.Module):
-    def __init__(self):
+class NCMML(torch.nn.Module):
+    def __init__(self, in_features: int, out_features: int, mean_features: torch.Tensor,
+                 transform: str = 'linear', init_method: str = 'random'):
         super().__init__()
+
+        if transform == 'linear':
+            self.transform = LinearTransform(
+                in_features=in_features, out_features=out_features, init_method=init_method)
+        else:
+            raise Exception(f'Unknown transform : {transform}')
+
+        self.mean_features = torch.tensor(mean_features)
         self.loss = nn.CrossEntropyLoss()
 
-    def forward(self,
-                features: torch.Tensor,
-                features_mean: torch.Tensor,
-                targets: torch.Tensor) -> torch.Tensor:
-        assert features.dim() == 2 and features_mean.dim() == 2
+    def forward(self, features: torch.Tensor, targets: torch.Tensor = None) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        features, mean_features = self.transform(
+            features), self.transform(self.mean_features.to(device=features.device))
+        negative_dists = -(features.unsqueeze(1) -
+                           mean_features.unsqueeze(0)).norm(dim=-1)
 
-        dists = (features.unsqueeze(1) -
-                 features_mean.unsqueeze(0)).norm(dim=-1)
-        return self.loss(-dists, targets)
+        outputs = (features, mean_features, negative_dists,)
+
+        if targets is not None:
+            loss = self.loss(negative_dists, targets)
+            outputs = (loss,) + outputs
+
+        return outputs
+
+    def predict(self, features: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        _, _, logits = self.forward(features)
+        _, indices = logits.topk(k=1, dim=-1)
+
+        return logits, indices
 
 
 class LinearTransform(nn.Module):
