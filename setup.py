f096621d4ceb485b97be1044537206278874e27a
Vinnam Kim <vinnamkim@ncsoft.com>
2022-08-15 15:36:29 +0900

    Fix
    Signed-off-by: Vinnam Kim <vinnamkim@ncsoft.com>

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="torchncmml",
     version="0.0.0",
-    author="Vinnam Kim",
-    author_email="vinnamkim@gmail.com",
-    description="A PyTorch implementation of Nearest Class Mean Metric Learning (NCMML)",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    keywords="pytorch metric-learning",
     license="MIT",
-    url="https://github.com/vinnamkim/PyTorchNCMML",
     packages=find_packages(
         exclude=['example']
     ),
